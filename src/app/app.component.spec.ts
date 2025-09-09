338c03807aa26092048222aefd525e9380afd86d
Ajay Yadav <ajay.y@margerp.net>
2025-09-10 01:10:44 +0530

    Updated code and fixes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Portfolio' title`, () => {
+  it(`should have the 'AjayPortfolio' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Portfolio');
+    expect(app.title).toEqual('AjayPortfolio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Portfolio');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AjayPortfolio');
   });
 });
